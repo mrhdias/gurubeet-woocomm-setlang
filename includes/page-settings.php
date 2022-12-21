35f9790ae9fb968d315874c929dcc0e9c7abd5f1
Henrique Dias <mrhdias@gmail.com>
2022-12-21 22:33:58 +0000

    New file with settings page
    Part of the settings page code that was removed from the file "woocomm-setlang.php"

---
+++ includes/page-settings.php
@@ -0,0 +1,28 @@
+<?php defined( 'ABSPATH' ) || exit; ?>
+<style>
+.gurubeet_woocomm_setlang_title {
+    /* border-bottom: 1px solid #aaa; */
+    /* padding-bottom: 10px; */
+    /* margin-right: 10px; */
+}
+.gurubeet_woocomm_setlang_form > table {
+    /* width: calc(100% - 10px); */
+    border-top: 1px solid #aaa;
+    border-bottom: 1px solid #aaa;
+    margin-bottom: 10px;
+}
+.gurubeet_woocomm_setlang_form > table > tbody {
+    /* border-bottom: 1px solid #aaa; */
+}
+</style>
+<div class="wrap">
+    <h2 class="gurubeet_woocomm_setlang_title">Gurubeet Woocomm Set Language Popup Plugin Settings</h2>
+    <h2 class="nav-tab-wrapper"><?php settings_errors(); ?></h2>
+    <form class="gurubeet_woocomm_setlang_form" action="options.php" method="post">
+        <?php
+        settings_fields( 'gurubeet_woocomm_setlang_plugin_options' );
+        do_settings_sections( 'gurubeet_woocomm_setlang_plugin' );
+        ?>
+        <input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e( 'Save' ); ?>" />
+    </form>
+</div>
