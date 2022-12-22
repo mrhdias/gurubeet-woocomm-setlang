82b154d571dbd8c376da687dce4c5701de5ded63
Henrique Dias <mrhdias@gmail.com>
2022-12-22 21:51:34 +0000

    Added an Id to the form and fixed the style sheets

--- includes/page-settings.php
+++ includes/page-settings.php
@@ -5,20 +5,20 @@
     /* padding-bottom: 10px; */
     /* margin-right: 10px; */
 }
-.gurubeet_woocomm_setlang_form > table {
+#gurubeet_woocomm_setlang_form > table {
     /* width: calc(100% - 10px); */
     border-top: 1px solid #aaa;
     border-bottom: 1px solid #aaa;
     margin-bottom: 10px;
 }
-.gurubeet_woocomm_setlang_form > table > tbody {
+#gurubeet_woocomm_setlang_form > table > tbody {
     /* border-bottom: 1px solid #aaa; */
 }
 </style>
 <div class="wrap">
     <h2 class="gurubeet_woocomm_setlang_title">Gurubeet Woocomm Set Language Popup Plugin Settings</h2>
     <h2 class="nav-tab-wrapper"><?php settings_errors(); ?></h2>
-    <form class="gurubeet_woocomm_setlang_form" action="options.php" method="post">
+    <form id="gurubeet_woocomm_setlang_form" action="options.php" method="post">
         <?php
         settings_fields( 'gurubeet_woocomm_setlang_plugin_options' );
         do_settings_sections( 'gurubeet_woocomm_setlang_plugin' );
